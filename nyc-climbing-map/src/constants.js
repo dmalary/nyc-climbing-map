437334c20a293e73d9f2589e5073fccd5beab3b4
madhat5 <davidmalary@gmail.com>
2026-07-22 10:16:07 -0400

    feat: add constants

---
+++ nyc-climbing-map/src/constants.js
@@ -0,0 +1,16 @@
+export const WIDTH = 800;
+export const HEIGHT = 900;
+
+export const LINE_STROKE_WIDTH = 6;
+export const TICK_INTERVAL = 24;      // small unlabeled dots
+export const TICK_RADIUS = 3;
+export const LABEL_INTERVAL = 60;     // labeled bullets, sparser than plain ticks
+export const LABEL_RADIUS = 9;
+export const TERMINUS_RADIUS = 10;
+export const INITIAL_ZOOM_SCALE = 1.7;
+export const DIMMED_OPACITY = 0.12;
+export const SELECTED_OPACITY = 1;
+export const DEFAULT_OPACITY = 0.85;
+export const BULLET_OFFSET = 14; 
+
+export const TIER_ORDER = { company: 0, climbType: 1, board: 2 };
